1b3be1a1181374203a3de0ed4707ae06e94c073a
Lucas Ferreira Da Silva <needslucas944@gmail.com>
2024-08-20 22:36:10 -0300

    build v1 - publicação primeira api rest em nuvem utilizando spring boot 3 e railway

---
+++ src/main/java/me/dio/spring_api_rest_em_nuvem_java_railway/SpringPublicandoApiRestEmNuvemJavaRailwayApplication.java
@@ -0,0 +1,13 @@
+package me.dio.spring_api_rest_em_nuvem_java_railway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPublicandoApiRestEmNuvemJavaRailwayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPublicandoApiRestEmNuvemJavaRailwayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/dio/spring_api_rest_em_nuvem_java_railway/SpringPublicandoApiRestEmNuvemJavaRailwayApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dio.spring_api_rest_em_nuvem_java_railway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPublicandoApiRestEmNuvemJavaRailwayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
