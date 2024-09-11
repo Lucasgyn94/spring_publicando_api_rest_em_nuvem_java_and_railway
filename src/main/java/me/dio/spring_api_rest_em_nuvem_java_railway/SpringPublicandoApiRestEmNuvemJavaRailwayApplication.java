da0aff26394143e3abc31121bd552ea204684792
Lucas Ferreira Da Silva <needslucas944@gmail.com>
2024-09-10 23:34:03 -0300

    Bugfix CORS

--- src/main/java/me/dio/spring_api_rest_em_nuvem_java_railway/SpringPublicandoApiRestEmNuvemJavaRailwayApplication.java
+++ src/main/java/me/dio/spring_api_rest_em_nuvem_java_railway/SpringPublicandoApiRestEmNuvemJavaRailwayApplication.java
@@ -1,9 +1,12 @@
 package me.dio.spring_api_rest_em_nuvem_java_railway;
 
 import io.github.cdimascio.dotenv.Dotenv;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(servers = {@Server(url = "/", description = "Default Server URL")})
 @SpringBootApplication
 public class SpringPublicandoApiRestEmNuvemJavaRailwayApplication {
 
